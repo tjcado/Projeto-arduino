2993ccdc5f8aa47f56e6a584099fc4a49203cc7b
tjcado <101905011+tjcado@users.noreply.github.com>
2022-05-15 15:12:39 -0300

    Update Automacao.js

--- Automacao.js
+++ Automacao.js
@@ -1,11 +1,11 @@
 function AlteraRele(){
     var estado = document.getElementById("estado").innerHTML;
-    if(estado === "0"){
-        document.getElementById("estado").innerHTML="1";
+    if(estado === "1"){
+        document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
 		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
     } else {
-        document.getElementById("estado").innerHTML="0";
+        document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
         document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
     }
