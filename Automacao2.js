883193577bceabcb5cbee4a4bc3e63e69b04a15c
tjcado <101905011+tjcado@users.noreply.github.com>
2022-05-20 00:15:53 -0300

    Update Automacao2.js

--- Automacao2.js
+++ Automacao2.js
@@ -13,13 +13,13 @@ function AlteraRele(){
 
 function Movimento(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
-    if(estado_2 === "0"){
-        document.getElementById("estado_2").innerHTML="1";
-        document.getElementById("movimento").innerHTML="<div class='movimento_sala'></div>";
-		document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
-    } else {
+    if(estado_2 === "1"){
         document.getElementById("estado_2").innerHTML="0";
-        document.getElementById("movimento").innerHTML="<div class='rele2_desligado'></div>";
-        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Liga</a>";
+        document.getElementById("movimento").innerHTML="<div class='movimento'></div>";
+	//document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
+    } else {
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("movimento").innerHTML="<div class='sem_movimento'></div>";
+        //document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Liga</a>";
     }
 }
