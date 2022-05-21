38a2c30230150e3ceee1c9c3bef9d644ef03259c
tjcado <101905011+tjcado@users.noreply.github.com>
2022-05-21 15:35:38 -0300

    Update Automacao2.js

--- Automacao2.js
+++ Automacao2.js
@@ -3,22 +3,22 @@ function AlteraRele(){
     if(estado === "0"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desligar</a>";
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Ligar</a>";
     }
 }
 
 function Movimento(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
-    if(estado_2 === "1"){
-        document.getElementById("estado_2").innerHTML="0";
+    if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
         document.getElementById("movimento").innerHTML="<div class='movimento'></div>";
 	document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>vazio</a>";
     } else {
-        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("estado_2").innerHTML="0";
         document.getElementById("movimento").innerHTML="<div class='sem_movimento'></div>";
         document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>movimento</a>";
     }
