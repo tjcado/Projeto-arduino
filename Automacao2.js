33147badb104751e1753d6d29585320fcc0670d4
tjcado <101905011+tjcado@users.noreply.github.com>
2022-05-22 12:13:18 -0300

    Update Automacao2.js

--- Automacao2.js
+++ Automacao2.js
@@ -14,13 +14,13 @@ function AlteraRele(){
 
 function AlteraMovimento(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
-    if(estado_2 === "1" ){
+    if(estado_2 === "0" ){
         document.getElementById("estado_2").innerHTML="1";
         document.getElementById("movit").innerHTML="<div class='sem_movi'></div>";
 	document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_mov'>vazio</a>";
     } else {
         document.getElementById("estado_2").innerHTML="0";
         document.getElementById("movit").innerHTML="<div class='movi'></div>";
-        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Aula</a>";
+        document.getElementById("botao_mov").innerHTML="<a href='/?2_ligar' class='botao_2'>Movimento</a>";
     }
 }
